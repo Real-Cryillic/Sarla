65c62cef4592e84a29c59304cdb3049809bdd099
Real-Cryillic <cunningham.dom@outlook.com>
2022-09-29 21:50:04 -0400

    formatting

--- sarla/server/server.py
+++ sarla/server/server.py
@@ -3,61 +3,70 @@ from prompt_toolkit.styles import Style
 from prompt_toolkit.completion import NestedCompleter
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
+# global variables
 session = PromptSession()
 host = "0.0.0.0"
 port = "443"
 auth_flag = "random"
 
-style = Style.from_dict({
-    # User input (default text).
-    '':          '#884444',
+# prompt color/style
+style = Style.from_dict(
+    {
+        # User input (default text)
+        "": "#884444",
+        # Prompt
+        "server": "#884444",
+        "pipe": "ansicyan",
+        "host": "ansired",
+        "port": "ansigreen",
+        "authflag": "ansimagenta",
+        "default": "#884444",
+    }
+)
 
-    # Prompt.
-    'server': '#884444',
-    'pipe': 'ansicyan',
-    'host': 'ansired',
-    'port': 'ansigreen',
-    'authflag': 'ansimagenta',
-    'default': '#884444'
-})
-
+# prompt message
 message = [
-    ('class:server', '( Sarla )'),
-    ('class:default', '( '),
-    ('class:host', host),
-    ('class:default', ' : '),
-    ('class:port', port),
-    ('class:default', ' : '),
-    ('class:authflag', auth_flag),
-    ('class:default', ' )'),
-    ('class:pipe',    ' > '),
+    ("class:server", "( Sarla )"),
+    ("class:default", "( "),
+    ("class:host", host),
+    ("class:default", " : "),
+    ("class:port", port),
+    ("class:default", " : "),
+    ("class:authflag", auth_flag),
+    ("class:default", " )"),
+    ("class:pipe", " > "),
 ]
 
-message_completer = NestedCompleter.from_nested_dict({
-    'help': None,
-    'set': {
-        'version': None,
-        'clock': None,
-        'ip': {
-            'interface': {'brief'}
-        }
-    },
-    'listen': None,
-    'quit': None,
-    'clear': None,
-})
+message_completer = NestedCompleter.from_nested_dict(
+    {
+        "help": None,
+        "set": {"version": None, "clock": None, "ip": {"interface": {"brief"}}},
+        "listen": None,
+        "quit": None,
+        "clear": None,
+    }
+)
+
 
 def quit():
     input = session.prompt("Are you sure you want to quit? (y/n) Default (y): ")
-    if (input.lower() == "n"):
+    if input.lower() == "n":
         return False
     else:
         return True
 
+
 def main():
-    print("\033c")
+    print("\033c")  # Clear current terminal
+
+    # CLI prompt handler
     while True:
-        input = session.prompt(message, style=style, completer=message_completer, auto_suggest=AutoSuggestFromHistory()).split()
+        input = session.prompt(
+            message,
+            style=style,
+            completer=message_completer,
+            auto_suggest=AutoSuggestFromHistory(),
+        ).split()
         try:
             if input[0] == "help":
                 print("help")
@@ -74,6 +83,7 @@ def main():
                 print("Invalid command")
         except IndexError:
             print("Enter a command or 'quit' to exit the program.")
+
 
 if __name__ == "__main__":
     main()
