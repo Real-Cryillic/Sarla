a1d74359f00d502db7dcea2ca0a7bc439cdea799
Cryillic <dac8484@rit.edu>
2022-11-21 13:44:36 -0500

    update dictionary references

--- sarla/server/src/registration.py
+++ sarla/server/src/registration.py
@@ -21,13 +21,17 @@ def register(data, dictionary):
     job_dictionary['user'] = agent_information[0]
     job_dictionary['host'] = agent_information[1]
     job_dictionary['pid'] = agent_information[2]
-    job_dictionary['build'] = agent_information[3]
+    job_dictionary['name'] = agent_information[3]
+    job_dictionary['domain'] = agent_information[4]
+    job_dictionary['build'] = agent_information[5]
     job_dictionary['keyword'] = agent_information[4]
 
-    key = generate_key(job_dictionary['keyword'])
+    if job_dictionary['keyword'] == keyword:
+        key = generate_key(job_dictionary['keyword'])
+        job_dictionary['key'] = key
 
-    job_dictionary['key'] = key
+        dictionary[job_dictionary['id']] = job_dictionary
+        return key
+    else:
+        return False
 
-    dictionary[job_dictionary['id']] = job_dictionary
-
-    return key
