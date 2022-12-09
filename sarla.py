7ddb29b010f24c718958365381d71aef046206fa
Real-Cryillic <cunningham.dom@outlook.com>
2022-12-09 15:08:21 -0500

    Initial refactor commit

--- sarla.py
+++ sarla.py
@@ -1,6 +1,21 @@
+from termcolor import colored 
+import click
 import sarla.server.server as server
-from sarla.server.src.utils.settings import init
+import sarla.client.client as client
+
+
+@click.command()
+@click.argument("status")
+def command(status):
+    if status == "client":
+        client.run()
+
+    elif status == "server":
+        server.run()
+
+    else:
+        click.echo(colored("Error: Argument unrecognized", "red"))
+
 
 if __name__ == "__main__":
-    init()
-    server.start()
+    command()
